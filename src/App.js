61e25ebb9b098784aa23ed7cb933a7317cfea40d
Markus Wedler <markuswedler8@gmail.com>
2023-01-09 23:07:37 +1000

    .

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     [ "Ctrl", "Win", "Alt", "", "Func", "Menu", "←", "↓", "→" ]
   ]
   const keysCode = [
-    [ "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Backspace"],
+    [ "Escape", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"],
     [ "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash"],
     [ "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter"],
     [ "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight","ArrowUp", "Delete"],
