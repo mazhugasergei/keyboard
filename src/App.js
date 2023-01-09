3be4d8362fc26515fc9cb0272f0175756d054ae4
Markus Wedler <markuswedler8@gmail.com>
2023-01-09 19:48:29 +1000

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+// react
+import { useEffect } from "react"
 
 function App() {
+  const rows = [
+    [ "Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Backspace" ],
+    [ "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{\n[", "}\n]", "|\n\\" ],
+    [ "Caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":\n;", "\"\n\'", "Enter" ],
+    [ "Shift", "Z", "X", "C", "V", "B", "N", "M", "<\n,", ">\n.", "?\n/", "Shift", "↑", "Del" ],
+    [ "Ctrl", "Win", "Alt", "", "Func", "Menu", "←", "↓", "→" ]
+  ]
+  const keysCode = [
+    [ "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Backspace"],
+    [ "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash"],
+    [ "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter"],
+    [ "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight","ArrowUp", "Delete"],
+    [ "ControlLeft", "MetaLeft", "AltLeft", "Space", "Function", "Menu", "ArrowLeft", "ArrowDown", "ArrowRight" ]
+  ]
+
+  const sound = new Audio('/assets/key.mp3')
+
+  const keyDown = key => {
+    sound.currentTime = 0
+    sound.play()
+    key.querySelector('.text').style.transform = "scale(.98)"
+    key.style.height = key.offsetHeight - 4 + "px"
+    key.style.minWidth = key.offsetWidth - 4 + "px"
+    key.style.margin = "4px"
+  }
+  const keyUp = key => {
+    key.querySelector('.text').style.transform = "scale(1)"
+    key.style.height = key.offsetHeight + 4 + "px"
+    key.style.minWidth = key.offsetWidth + 4 + "px"
+    key.style.margin = 0
+  }
+
+  useEffect(()=>{
+    // define all the keys
+    const keys = document.querySelectorAll('.key')
+    // animation on click
+    keys.forEach(key => {
+      key.addEventListener("mousedown", (e) => keyDown(e.target))
+      key.addEventListener("click", (e) => keyUp(e.target))
+    })
+    document.addEventListener("keydown", (e) => {
+      keyDown(document.querySelector('[data-key='+ e.code))
+    })
+    document.addEventListener("keyup", (e) => {
+      keyUp(document.querySelector('[data-key='+ e.code))
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="keyboard">
+        <div className="cont">
+          {
+            rows.map((row, i) => (
+              <div className="row" key={i}>
+                {
+                  row.map((key, j) => (
+                    <div className="key" data-key={keysCode[i][j]} key={i+"_"+j}>
+                      <div className="top">
+                        <div className="text">{ key }</div>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
